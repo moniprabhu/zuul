5f1a680255d4f7b0c88e4ab3be8b6cc44da8f941
Monishaa <monishaa2701@gmail.com>
2019-05-28 16:33:19 +0530

    inital commit

---
+++ microservices/src/main/java/com/stackroute/microservices/MicroservicesApplication.java
@@ -0,0 +1,34 @@
+package com.stackroute.microservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+@EnableZuulProxy
+public class MicroservicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroservicesApplication.class, args);
+	}
+
+
+	@Bean
+	public PreFilter preFilter() {
+		return new PreFilter();
+	}
+	@Bean
+	public PostFilter postFilter() {
+		return new PostFilter();
+	}
+	@Bean
+	public ErrorFilter errorFilter() {
+		return new ErrorFilter();
+	}
+	@Bean
+	public RouteFilter routeFilter() {
+		return new RouteFilter();
+	}
+
+}
