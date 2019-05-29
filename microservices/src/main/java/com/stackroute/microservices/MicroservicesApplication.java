1bc2f87d86b4408d636fb876244fbd2c047057e2
Monishaa <monishaa2701@gmail.com>
2019-05-29 17:49:28 +0530

    added config server

--- microservices/src/main/java/com/stackroute/microservices/MicroservicesApplication.java
+++ microservices/src/main/java/com/stackroute/microservices/MicroservicesApplication.java
@@ -1,12 +1,18 @@
 package com.stackroute.microservices;
 
+import com.stackroute.microservices.filters.ErrorFilter;
+import com.stackroute.microservices.filters.PostFilter;
+import com.stackroute.microservices.filters.PreFilter;
+import com.stackroute.microservices.filters.RouteFilter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @EnableZuulProxy
+@EnableEurekaClient
 public class MicroservicesApplication {
 
 	public static void main(String[] args) {
